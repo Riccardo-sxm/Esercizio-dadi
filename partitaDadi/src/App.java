3e899b5316976bc316a2e11dc5e53cdbef2fc287
Riccardo-sxm <riccardodinni@gmail.com>
2024-12-06 10:31:15 +0100

    upload finale

--- partitaDadi/src/App.java
+++ partitaDadi/src/App.java
@@ -28,21 +28,22 @@ public class App {
                     System.out.println("Non puoi scommettere una somma minore o uguale a 0!");
                 }
             } while (budgetScommesso > budgetTotale || budgetScommesso <= 0);
-
+            System.out.println("----------------------------------------------------------");
             // creo il ciclo dei tre turni
             for (int turno = 0; turno < 3; turno++) {
                 
                     dado1 = rnd.nextInt(6) + 1;
                     dado2 = rnd.nextInt(6) + 1;
                     sommadadi = dado1 + dado2;
-                    System.out.println("Hai fatto: " + sommadadi); // faccio visualizzare la somma dei tiri dell'utente
+                    System.out.println("Hai fatto: "+ dado1 + " e " + dado2 +" ("+ sommadadi + ")"); // faccio visualizzare la somma dei tiri dell'utente
                     dadopc1 = rnd.nextInt(6) + 1;
                     dadopc2 = rnd.nextInt(6) + 1;
                     sommadadipc = dadopc1 + dadopc2;
-                    System.out.println("Il pc ha fatto: " + sommadadipc); // faccio visualizzare la somma dei tiri del pc
+                    System.out.println("Il pc ha fatto: "+ dadopc1 + " e " + dadopc2 +" ("+ sommadadipc + ")"); // faccio visualizzare la somma dei tiri del pc
 
                     if (sommadadi == sommadadipc)
                         System.out.println("Hai pareggiato."); // in caso di pareggio il punteggio rimane invariato
+                  
                     else {
                         if (sommadadi > sommadadipc) {
                             System.out.println("Hai vinto!");
@@ -51,7 +52,8 @@ public class App {
                             System.out.println("Il pc ha vinto.");
                             puntiPC = puntiPC + 1; // in caso di sconfitta aumenta il punteggio del PC
                         }
-                    }                                            
+                    }          
+                    System.out.println();                                  
                 
             }
             
@@ -62,21 +64,23 @@ public class App {
                 if (puntiUtente > puntiPC) {
                     System.out.println("Hai vinto la partita!");
                     budgetTotale = budgetTotale + budgetScommesso ;  //incremento il budget in caso di vincita
-                    System.out.println("Bravo! Hai vinto " + budgetScommesso + "euro. Il tuo budget è di " + budgetTotale + " euro "); //
+                    System.out.println("Bravo! Hai vinto " + budgetScommesso + " euro. Il tuo budget è di " + budgetTotale + " euro "); //
                 } else {
                     System.out.println("Hai perso la partita!");
                     budgetTotale = budgetTotale - budgetScommesso;  //decremento il budget in caso di perdita
-                    System.out.println("Mi dispiace! Hai perso " + budgetScommesso + "euro. Il tuo budget è di " + budgetTotale + " euro ");
+                    System.out.println("Mi dispiace! Hai perso " + budgetScommesso + " euro. Il tuo budget è di " + budgetTotale + " euro ");
                 }
+                System.out.println("----------------------------------------------------------");
             }
 
             if (budgetTotale <=0) {
                 cont = false;
                 System.out.println("Il tuo budget è esaurito!");
             } else {
-            System.out.println("Vuoi continuare a giocare?(true/false)");
+            System.out.println("Vuoi continuare a giocare? (true/false)");
             cont = sc.nextBoolean();
         }
+        System.out.println();
            
         }
         System.out.println("");
